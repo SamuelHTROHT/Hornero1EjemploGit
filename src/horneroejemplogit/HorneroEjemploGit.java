416d231826cee6cd5faebace0dfa156dfbb6e204
damt113 <damt113@36-24.aglinformatica.es>
2022-05-05 16:08:51 +0200

    version 1

---
+++ src/horneroejemplogit/HorneroEjemploGit.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package horneroejemplogit;
+
+/**
+ *
+ * @author damt113
+ */
+public class HorneroEjemploGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Samuel Hornero");
+        System.out.println("Version1");
+    }
+    
+}
